daf783e473637868e48fa416f9e2b76902d2da38
Ale <castorina387@gmail.com>
2023-06-09 23:44:59 +0200

    added crafting recipes

--- mod/init.lua
+++ mod/init.lua
@@ -1,6 +1,13 @@
 FUMOS = {
-    "cirno", "reimu", "marisa", "alice", "patchouli"
+    "cirno", "reimu"
 }
+
+SPECIALITEMS = {"default:snow", "default:gold_ingot"}
+
+minetest.register_craftitem("fumoplushies:plushie", {
+	description = "A base for a fumo",
+	inventory_image = "fumobaseitem.png"
+})
 
 for i = 1, #FUMOS do
     minetest.register_node("fumoplushies:" .. FUMOS[i] .. "plushie", {
@@ -9,6 +16,8 @@ for i = 1, #FUMOS do
         mesh = "fumo" .. FUMOS[i] .. ".obj",
         tiles = {"fumo" .. FUMOS[i] .. ".png"},
         paramtype2 = "facedir",
+        paramtype = "light",
+	sunlight_propagates = true,
         selection_box = {
             type= "fixed",
             fixed = {-0.3, -0.46, -0.4, 0.3, 0.3, 0.3}
@@ -19,7 +28,23 @@ for i = 1, #FUMOS do
         },
         is_ground_content = false,
         groups = {snappy = 2, choppy = 2, oddly_breakable_by_hand = 3,
-        flammable = 3, wool = 1}
+        flammable = 3, wool = 1},
+    })
+end
+-------------------------------------------------------------------------
+minetest.register_craft({
+    output =  "fumoplushies:plushie 1",
+    recipe = {
+    	{"", "farming:string ", ""},
+    	{"wool:white", "wool:white", "wool:white"},
+    	{"", "wool:white", ""}
+    }
+})
+for i = 1, #FUMOS do
+    minetest.register_craft({
+        type = "shapeless",
+        output =  "fumoplushies:" .. FUMOS[i] .. "plushie 1",
+        recipe = {"fumoplushies:plushie",SPECIALITEMS[i]}
     })
 end
 
